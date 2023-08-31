bbee81cf8803be8f79438bd1e1535372eec4314f
anunna <anunna@lilitusec.net>
2023-08-31 22:59:01 +0300

    starting code

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float train[][2] = {
+    {0, 0},
+    {1, 2},
+    {2, 4},
+    {3, 6},
+    {4, 8},
+};
+
+// y = x*w;
+
+int main()
+{
+    printf("Hello, Seaman!\n");
+    return 0;
+}
