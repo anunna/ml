e68fc1a6e389af49a80a03b6baf4c7ab98293430
anunna <anunna@lilitusec.net>
2023-09-15 13:17:55 +0300

    some more c work

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ float train[][2] = {
     {3, 6},
     {4, 8},
 };
+#define train_count (sizeof(train)/sizeof(train[0])
 
 float rand_float(void)
 {
@@ -19,8 +20,18 @@ int main()
 {
     // srand(time(0));
     srand(69);
-    // y = x*w;
     float w = rand_float()*10.0f;
-    printf("%f\n", w);
+
+    float result = 0.0f;
+    for (size_t i = 0; i < train_count; ++i) {
+        float x = train[i][0];
+        float y = x*w;
+        float d = y - train[i][1];
+        result += d*d;
+    }
+    result /= train_count;
+
+    printf("%f\n", result);
+
     return 0;
 }
